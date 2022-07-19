8a3f4d7d7648c8a362db69b7863309b5abb099e3
Andrej Nankov <andrejnankov@gmail.com>
2022-07-19 16:19:38 +0200

    clean up

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# tutor
-An app for sharing exercises, books, scripts with students.  
+# Vue 3 + Vite
+
+This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+
+## Recommended IDE Setup
+
+- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
+
+## Dev tools
+- bootstrap
+- pinia state manager
+- vite (vue 3)
---
+++ web/src/atomic/organisms/CardHorizontal.vue
@@ -0,0 +1,25 @@
+<template>
+  <router-link class="card mb-3 border-0 shadow text-decoration-none" to="/students/2/lectures/2">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="https://picsum.photos/200/300" class="img-fluid rounded-start" alt="..." />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h4 class="card-title text-dark">Card title</h4>
+          <p class="card-text text-dark small">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <p class="card-text">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {};
+</script>
